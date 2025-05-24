37e9b3f1b3cedd7897569651c8170bdad47ed895
KylerCWK <kylergelissen@gmail.com>
2025-05-23 21:53:16 -0400

    Implement QR Code generation in QRCodeGenerator component and update package.json dependencies

--- project/client/src/components/QRCodeGenerator.vue
+++ project/client/src/components/QRCodeGenerator.vue
@@ -1,14 +1,33 @@
 <template>
   <div>
     <h1>QR Code Generator</h1>
-    <!-- Placeholder -->
-    <p>This is a placeholder for the QR Code Generator component.</p>
+    <canvas id="qrcode"></canvas>
   </div>
+
 </template>
 
 <script>
+import QRCode from "qrcode";
+
 export default {
   name: 'QRCodeGenerator',
+  mounted() {
+    QRCode.toCanvas(
+      document.getElementById("qrcode"),
+      "https://www.pizzahut.com/", //You should be able to change this to any URL
+      {
+        width: 128,
+        color: {
+          dark: "#000000",
+          light: "#ffffff"
+        },
+        errorCorrectionLevel: "H"
+      },
+      function (error) {
+        if (error) console.error(error);
+      }
+    );
+  }
 };
 </script>
 
