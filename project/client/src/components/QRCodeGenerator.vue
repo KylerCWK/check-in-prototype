84491f7d1110fa514d671223b437e96ec1806701
KylerCWK <kylergelissen@gmail.com>
2025-05-24 00:33:21 -0400

    Refactor QRCodeGenerator component to include input for dynamic QR code generation and update canvas rendering method

--- project/client/src/components/QRCodeGenerator.vue
+++ project/client/src/components/QRCodeGenerator.vue
@@ -1,38 +1,59 @@
 <template>
-  <div>
-    <h1>QR Code Generator</h1>
-    <canvas id="qrcode"></canvas>
-  </div>
-
+    <div>
+        <h1>QR Code Generator</h1>
+        <input v-model="inputValue" placeholder="Enter text or URL" @input="generateQRCode" />
+        <canvas ref="qrcode"></canvas>
+    </div>
 </template>
 
 <script>
 import QRCode from "qrcode";
 
-export default {
-  name: 'QRCodeGenerator',
-  mounted() {
-    QRCode.toCanvas(
-      document.getElementById("qrcode"),
-      "https://www.pizzahut.com/", //You should be able to change this to any URL
-      {
-        width: 128,
-        color: {
-          dark: "#000000",
-          light: "#ffffff"
-        },
-        errorCorrectionLevel: "H"
-      },
-      function (error) {
-        if (error) console.error(error);
-      }
-    );
-  }
+export default{
+    name: 'QRCodeGenerator',
+    data(){
+        return{
+            inputValue: ''
+        };
+    },
+
+    methods:{
+        generateQRCode(){
+            QRCode.toCanvas(
+                this.$refs.qrcode,
+                this.inputValue || ' ', //blank if empty
+                {
+                    width: 128,
+                    color:{
+                        dark: "#000000",
+                        light: "#ffffff"
+                    },
+                    errorCorrectionLevel: "H"
+                },
+
+                function (error){
+                    if(error){
+                        console.error(error);
+                    }
+                }
+            );
+        }
+    },
+
+    mounted(){
+        this.generateQRCode();
+    }
 };
 </script>
 
+//Let's add some basic styles later
 <style scoped>
 h1 {
-  color: #333;
+    color: #333;
+}
+input {
+    margin-bottom: 10px;
+    padding: 5px;
+    width: 250px;
 }
 </style>
