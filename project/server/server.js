ed2a461b37fe9247cf39fc4359bb312a34e511be
KylerCWK <kylergelissen@gmail.com>
2025-05-27 17:47:28 -0400

    Update server port configuration to use environment variable

--- project/server/server.js
+++ project/server/server.js
@@ -18,7 +18,8 @@ mongoose.connect(process.env.MONGO_URI, { dbName: 'qrlibrary' })
   .catch((err) => console.error(err));
 
 app.get('/', (req, res) => res.send('Hello from backend!'));
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Mount auth routes from routes/auth.js for /api/auth endpoints
